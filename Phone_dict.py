9e75ee751505a2026a3c2c601b0a159a1cad145e
generic <generic@generic-pc.(none)>
2014-08-19 23:03:59 -0400

    Optimization attempt with dictionary

---
+++ Phone_dict.py
@@ -0,0 +1,106 @@
+import sys
+import os
+
+def create_phonebook(phonebook_name):
+    filename = '%s.txt' % phonebook_name
+    if os.path.exists(filename):
+        raise Exception("That Phonebook already exists!")
+    with open(filename, 'w') as f:
+        pass    
+
+def add_entry(name, number, phonebook_name):
+    if not os.path.exists('%s.txt' % phonebook_name):
+        raise Exception("That Phonebook does not exist!")
+    with open('%s.txt' % phonebook_name, 'r') as f:
+        for line in f: 
+            entry_name, entry_number = line.strip().split('\t')
+            if entry_name == name:
+                raise Exception('That Entry Already Exists! Please use the Update function!')
+        
+    with open('%s.txt' % phonebook_name, 'a') as f:
+        f.write('%s\t%s' % (name, number))
+        f.write('\n')
+        pass    
+
+def remove_entry(name, phonebook_name):
+    name_list = []
+    number_list = []
+    if not os.path.exists('%s.txt' % phonebook_name):
+        raise Exception("That Phonebook does not exist!")
+    with open('%s.txt' % phonebook_name, 'r') as f:
+        for line in f: 
+            entry_name, entry_number = line.strip().split('\t')
+            if entry_name != name:
+                name_list.append(entry_name)
+                number_list.append(entry_number)
+                
+    with open('%s.txt' % phonebook_name, 'w') as f:
+        pass
+    for z in range(len(name_list)):
+        add_entry(name_list[z], number_list[z], phonebook_name)
+        pass
+
+def lookup_entry(name, phonebook_name):
+    if not os.path.exists('%s.txt' % phonebook_name):
+        raise Exception("That Phonebook does not exist!")
+    with open('%s.txt' % phonebook_name, 'r') as f:
+        for line in f: 
+            entry_name, entry_number = line.strip().split('\t')
+            if entry_name == name:
+                print entry_name, entry_number
+        pass    
+
+def update_entry(name, number, phonebook_name):
+    
+    name_list = []
+    number_list = []
+    if not os.path.exists('%s.txt' % phonebook_name):
+        raise Exception("That Phonebook does not exist!")
+    with open('%s.txt' % phonebook_name, 'r') as f:
+        for line in f: 
+            entry_name, entry_number = line.strip().split('\t')
+            if entry_name != name:
+                name_list.append(entry_name)
+                number_list.append(entry_number)
+                
+    with open('%s.txt' % phonebook_name, 'w') as f:
+        pass
+    for z in range(len(name_list)):
+        add_entry(name_list[z], number_list[z], phonebook_name)
+    with open('%s.txt' % phonebook_name, 'a') as f:
+        f.write('%s\t%s' % (name, number))
+        f.write('\n')
+        pass        
+
+def reverse_lookup_entry(number, phonebook_name):
+    if not os.path.exists('%s.txt' % phonebook_name):
+        raise Exception("That Phonebook does not exist!")
+    with open('%s.txt' % phonebook_name, 'r') as f:
+        for line in f: 
+            entry_name, entry_number = line.strip().split('\t')
+            if entry_number == number:
+                print entry_name, entry_number
+        pass   
+
+if __name__ == "__main__":
+    arguments = sys.argv[:]
+    script = arguments.pop(0)
+    if not arguments:
+        raise Exception ('Command required')
+        
+    command = arguments.pop(0)
+
+    command_funcs = {
+        'create' : create_phonebook,  
+        'add' : add_entry,
+        'update' : update_entry,
+        'remove' : remove_entry,
+        'lookup' : lookup_entry,
+        'reverse-lookup' : reverse_lookup_entry
+    }
+
+    func = command_funcs[command] 
+    
+    func(*arguments)   
+
+
